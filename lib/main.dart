fb2e94af91cc76ad651b38ef44b63e331734eb76
GuiQRSM <endbeob15@gmail.com>
2021-11-10 16:26:31 -0300

    Imports material.dart, and adds a Container.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:flutter/material.dart';
 
 void main() {
 
+ runApp(
+   MaterialApp(
+     title: "E-conomiz",
+     debugShowCheckedModeBanner: false,
+     home: Container(
+       //color: Colors.grey,
+       padding: const EdgeInsets.fromLTRB(40, 50, 40, 50),
+       margin: const EdgeInsets.only(top:40),
+       decoration: BoxDecoration(
+         border: Border.all(width: 5, color: Colors.blueGrey)
+       ),
 
+     ),
+   )
+ );
 
 }

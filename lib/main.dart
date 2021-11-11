c04ea74150697273574c63b1e8796c1648780a4c
GuiQRSM <endbeob15@gmail.com>
2021-11-11 17:24:05 -0300

    Implements a base layout structure using the Scaffold() class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,89 @@
 import 'dart:ui';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+const primeColor = Color.fromARGB(100, 98, 0, 238);
 
  runApp(
    MaterialApp(
      title: "E-conomiz",
      debugShowCheckedModeBanner: false,
-     home: Container(
-       //color: Colors.grey,
-       padding: const EdgeInsets.fromLTRB(20, 50, 20, 50),
-       margin: const EdgeInsets.only(top:40),
-       decoration: BoxDecoration(
-         border: Border.all(width: 5, color: Colors.blueGrey)
+     home: Scaffold(
+       appBar: AppBar(
+         title: Text("E-conomiz"),
+         backgroundColor: primeColor,
        ),
-       child: Column(
-         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-         crossAxisAlignment: CrossAxisAlignment.start,
-         children: const <Widget>[
-           Text(
-               "E-conomiz coming soon",
-             style: TextStyle(
-               fontSize: 35,
-               fontStyle: FontStyle.normal,
-               fontWeight: FontWeight.w200,
-               color: Colors.blueGrey,
-               decorationStyle: TextDecorationStyle.wavy,
-               decorationColor: Colors.grey,
-               letterSpacing: 3,
+       body: Padding(
+         padding: EdgeInsets.all(16),
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.start,
+           crossAxisAlignment: CrossAxisAlignment.start,
+           children: <Widget>[
+             Text(
+                 "E-conomiz",
+               style: TextStyle(
+                 fontSize: 30,
+                 fontWeight: FontWeight.w300,
+                 color: primeColor,
+               ),
              ),
-             textAlign: TextAlign.center,
+             Text(
+                 "Coming Soon!",
+               style: TextStyle(
+                 fontSize: 15,
+                 fontWeight: FontWeight.w700,
+                 color: Colors.purple,
+               ),
+             )
+           ],
+         ),
+       ),
+       bottomNavigationBar: BottomAppBar(
+         color: primeColor,
+         child: Padding(
+           padding: EdgeInsets.all(16),
+           child: Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: <Widget>[
+               Text(
+                   "b1",
+           style: TextStyle(
+             fontSize: 15,
+             fontWeight: FontWeight.w400,
+             color: Colors.white,
            ),
-         ],
+               ),
+               Text(
+                   "b2",
+                 style: TextStyle(
+                   fontSize: 15,
+                   fontWeight: FontWeight.w400,
+                   color: Colors.white,
+                 ),
+               ),
+               Text(
+                   "b3",
+                 style: TextStyle(
+                   fontSize: 15,
+                   fontWeight: FontWeight.w400,
+                   color: Colors.white,
+                 ),
+               ),
+               Text(
+                   "b4",
+                 style: TextStyle(
+                   fontSize: 15,
+                   fontWeight: FontWeight.w400,
+                   color: Colors.white,
+                 ),
+               )
+             ],
+           ),
+         ),
        ),
      ),
-   )
+   ),
  );
 
 }

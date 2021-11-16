7de41d7e2eba7b9a80339b6d48b6a3f814e8a986
GuiQRSM <endbeob15@gmail.com>
2021-11-16 17:55:32 -0300

    Move the entire structure of the stful HomeStateFul into the created Dart.file and instantiate this class inside the MaterialApp() home attribute in main.dart.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
+import 'package:economiz/HomeStateFul.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
+
 
 void main() {
 
@@ -15,135 +16,7 @@ void main() {
 
 }
 
-class HomeStateFul extends StatefulWidget {
-  const HomeStateFul({Key? key}) : super(key: key);
 
-  @override
-  _HomeStateFulState createState() => _HomeStateFulState();
-}
-
-const primeColor = Color.fromARGB(100, 98, 0, 238);
-
-
-class _HomeStateFulState extends State<HomeStateFul> {
-
-  var _content = [
-    "Pesquise preços sem sair de casa!",
-    "Monte sua lista de compras direto no app.",
-    "Atualização de preços, da sua lista automatica!",
-    "Compartilhe os melhores preços."
-  ];
-
-  var _text = "Test Content!";
-
-  void _callContent() {
-    var randomContent = Random().nextInt(_content.length);
-
-    setState(() {
-      _text = _content[randomContent];
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("E-conomiz"),
-        backgroundColor: primeColor,
-      ),
-      body: Container(
-        width: double.infinity,
-        child: Padding(
-          padding: EdgeInsets.all(17),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                "E-conomiz",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w400,
-                  color: primeColor,
-                ),
-              ),
-              Text(
-                "Coming Soon!",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.purple,
-                ),
-              ),
-              RaisedButton(
-                onPressed: _callContent,
-                color: primeColor,
-                child: Text(
-                  "Clique Aqui!",
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontWeight: FontWeight.w400,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              Text(
-                "$_text",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w700,
-                  color: primeColor,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: primeColor,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Text(
-                "b1",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "b2",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "b3",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              ),
-              Text(
-                "b4",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
 
 
 /*Scaffold(

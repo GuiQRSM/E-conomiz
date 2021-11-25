ce968b25dba094399ad892cca6929d5b9a858a4d
GuiQRSM <endbeob15@gmail.com>
2021-11-25 17:10:28 -0300

    Changes the String content of the Text() class, and of the two TextField() classes.

--- lib/HomeStateFul.dart
+++ lib/HomeStateFul.dart
@@ -32,79 +32,81 @@ class _HomeStateFulState extends State<HomeStateFul> {
         backgroundColor: primeColor,
       ),
       body: Container(
-        padding: EdgeInsets.all(32),
         width: double.infinity,
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.only(bottom: 32),
-              child: Image.asset(
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 32),
+                child: Image.asset(
                   "imagens/E-conomiz.png",
-              height: 120,),
-            ),
-            Padding(
-              padding: EdgeInsets.only(bottom: 10),
-              child: Text(
-                  "Faça seu pré-registro!",
-              style: TextStyle(
-                fontSize: 23,
-                fontWeight: FontWeight.w500,
-                color: primeColor,
-              ),),
-            ),
-            TextField(
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                labelStyle: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: primeColor,
+                  height: 120,),
+              ),
+              Padding(
+                padding: EdgeInsets.only(bottom: 10),
+                child: Text(
+                  "Balanço de preços",
+                  style: TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),),
+              ),
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Preço primário, ex 2.50",
+                  labelStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),
                 ),
-              ),
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w300,
-                color: primeColor,
-              ),
-              controller: _controllerEmail,
-            ),
-            TextField(
-              keyboardType: TextInputType.number,
-              decoration: InputDecoration(
-                labelText: "Número de telefone",
-                labelStyle: TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
-                  color: primeColor,
-                ),
-              ),
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w300,
-                color: primeColor,
-              ),
-              controller: _controllerPhoneNumber,
-            ),
-            Padding(
-                padding: EdgeInsets.only(top: 10),
-            child:RaisedButton(
-              onPressed: (){
-                print("E-mail: " + _controllerEmail.text);
-                print("Número de telefone: " + _controllerPhoneNumber.text);
-              },
-              padding: EdgeInsets.all(16),
-              color: primeColor,
-              textColor: Colors.white,
-              child: Text(
-                "Registrar",
                 style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w400,
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                  color: primeColor,
                 ),
+                controller: _controllerEmail,
               ),
-            ),),
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço secundário, ex 2.50",
+                  labelStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),
+                ),
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w300,
+                  color: primeColor,
+                ),
+                controller: _controllerPhoneNumber,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+                child:RaisedButton(
+                  onPressed: (){
+                    print("E-mail: " + _controllerEmail.text);
+                    print("Número de telefone: " + _controllerPhoneNumber.text);
+                  },
+                  padding: EdgeInsets.all(16),
+                  color: primeColor,
+                  textColor: Colors.white,
+                  child: Text(
+                    "Registrar",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),),
+            ],
+          ),
         ),
       ),
       bottomNavigationBar: BottomAppBar(

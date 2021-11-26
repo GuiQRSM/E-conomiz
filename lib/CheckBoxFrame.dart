8c81af682132d1b7ac8a8ed9605d5afb36e1150c
GuiQRSM <endbeob15@gmail.com>
2021-11-26 13:22:02 -0300

    It implements in the Scaffold() of the CheckBoxFrame() class of the stful structure, configurations of checkbox state changes in the onChanged attribute of each CheckBox() class and also configures visual aspects in each CheckBox() class. It also implements a new Padding() that takes a RaiedBotton().

--- lib/CheckBoxFrame.dart
+++ lib/CheckBoxFrame.dart
@@ -25,16 +25,97 @@ class _CheckBoxFrameState extends State<CheckBoxFrame> {
         backgroundColor: primeColor,
       ),
       body: Container(
-        padding: EdgeInsets.all(18),
+        padding: EdgeInsets.all(16),
         width: double.infinity,
         child: Column(
           children: <Widget>[
+            Text("Categorias",
+              style: TextStyle(
+                fontSize: 23,
+                fontWeight: FontWeight.w500,
+                color: primeColor,
+              ),),
             Padding(padding: EdgeInsets.only(top: 10),
-            child: CheckboxListTile(value: _selectedCereais, onChanged: null),),
+            child: CheckboxListTile(
+              title: Text("Cereais",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w500,
+                color: primeColor,
+              ),),
+                subtitle: Text("Milho, Trigo, Soja, Mostarda, Feijão, etc.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),),
+                activeColor: primeColor,
+                value: _selectedCereais,
+                onChanged: (bool? vlr1){
+                setState(() {
+                  _selectedCereais = vlr1!;
+                });
+                },
+             ),
+            ),
             Padding(padding: EdgeInsets.only(top: 10),
-            child: CheckboxListTile(value: _selectedHigiene, onChanged: null),),
+            child: CheckboxListTile(
+             title: Text("Higiene",
+               style: TextStyle(
+                 fontSize: 18,
+                 fontWeight: FontWeight.w500,
+                 color: primeColor,
+               ),),
+                subtitle: Text("Sabonete, Creme Dental, Papel higienico, etc.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),),
+                activeColor: primeColor,
+                value: _selectedHigiene,
+                onChanged: (bool? vlr2){
+               setState(() {
+                 _selectedHigiene = vlr2!;
+               });
+                },
+             ),
+            ),
             Padding(padding: EdgeInsets.only(top: 10, bottom: 10),
-            child: CheckboxListTile(value: _selectedBebidas, onChanged: null),)
+            child: CheckboxListTile(
+              title: Text("Bebidas",
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                  color: primeColor,
+                ),),
+                subtitle: Text("Refrigerante, Destilados, Cerveja, Energético, etc.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.black,
+                  ),),
+                activeColor: primeColor,
+                value: _selectedBebidas,
+                onChanged: (bool? vlr3){
+                setState(() {
+                  _selectedBebidas = vlr3!;
+                });
+                },
+             ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 10),
+            child:  RaisedButton(
+              padding: EdgeInsets.all(14),
+              onPressed: (){},
+              child: Text("Registrar",
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),),
+              color: primeColor,
+            ),),
           ],
         ),
       ),

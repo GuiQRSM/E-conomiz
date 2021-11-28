6010bd8508b5b32fa43af22343cd6d30dd6b9700
GuiQRSM <endbeob15@gmail.com>
2021-11-28 14:42:46 -0300

    Implements in RadioButton Frame's Scaffold(), inside the attribute declares four RadioListTile() classes, with their attributes configured and implements a Text() inside a Padding() class to display main text on the body..

--- lib/RadioButtonFrame.dart
+++ lib/RadioButtonFrame.dart
@@ -10,6 +10,9 @@ class RadioButtonFrame extends StatefulWidget {
 const primeColor = Color.fromARGB(255, 98, 0, 238);
 
 class _RadioButtonFrameState extends State<RadioButtonFrame> {
+
+  var _selcterRadio = "";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +25,66 @@ class _RadioButtonFrameState extends State<RadioButtonFrame> {
         width: double.infinity,
         child: Column(
           children: <Widget>[
+            Padding(padding: EdgeInsets.only(top: 13, bottom: 16),
+            child: Text("Favorite seu supermecado",
+            style: TextStyle(
+              fontSize: 23,
+              fontWeight: FontWeight.w500,
+              color: primeColor,
+            ),),
+            ),
+           RadioListTile(
+               title: Text("Líder",
+               style: TextStyle(
+                 fontSize: 17,
+                 fontWeight: FontWeight.w500,
+                 color: primeColor,
+               ),),
+               value: "Líder",
+               groupValue: _selcterRadio,
+               onChanged: (String? mrc1){
 
+               }
+           ),
+            RadioListTile(
+                title: Text("Mateus",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),),
+                value: "Mateus",
+                groupValue: _selcterRadio,
+                onChanged: (String? mrc2){
+
+                }
+            ),
+            RadioListTile(
+                title: Text("Formosa",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),),
+                value: "Formosa",
+                groupValue: _selcterRadio,
+                onChanged: (String? mrc3){
+
+                }
+            ),
+            RadioListTile(
+                title: Text("Preço Baixo",
+                  style: TextStyle(
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),),
+                value: "Preço Baixo",
+                groupValue: _selcterRadio,
+                onChanged: (String? mrc4){
+
+                }
+            ),
           ],
         ),
       ),

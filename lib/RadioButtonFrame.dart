970b42b138860bda90c40713e92475fbbbd35a5b
GuiQRSM <endbeob15@gmail.com>
2021-11-28 14:02:20 -0300

    Calls the stful structure in the RadioButton Frame dart file and declares the class named 'RadioButton Frame' in this same stful structure.

--- lib/RadioButtonFrame.dart
+++ lib/RadioButtonFrame.dart
@@ -1,1 +1,15 @@
 import 'package:flutter/material.dart';
+
+class RadioButtonFrame extends StatefulWidget {
+  const RadioButtonFrame({Key? key}) : super(key: key);
+
+  @override
+  _RadioButtonFrameState createState() => _RadioButtonFrameState();
+}
+
+class _RadioButtonFrameState extends State<RadioButtonFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

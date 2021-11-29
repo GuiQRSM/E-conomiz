8877b073bcd64daf7154db72d407729c07edc4af
GuiQRSM <endbeob15@gmail.com>
2021-11-29 17:07:03 -0300

    Declare variable _choiseUser2 in stful to set the value and onChanged attribute of one of the SwitchListTile() classes.

--- lib/SwitchFrame.dart
+++ lib/SwitchFrame.dart
@@ -12,6 +12,7 @@ const primeColor = Color.fromARGB(255, 98, 0, 238);
 class _SwitchFrameState extends State<SwitchFrame> {
 
   var _choiseKey = false;
+  var _choiseKey2 = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,19 +22,51 @@ class _SwitchFrameState extends State<SwitchFrame> {
         backgroundColor: primeColor,
       ),
       body: Container(
-        padding: EdgeInsets.all(14),
+        padding: EdgeInsets.all(10),
         width: double.infinity,
         child: Column(
           children: <Widget>[
            Padding(
-               padding: EdgeInsets.only(top: 14),
+               padding: EdgeInsets.only(bottom: 14),
            child:  Text("Configurações gerais",
              style: TextStyle(
-               fontSize: 20,
+               fontSize: 22,
                fontWeight: FontWeight.w500,
                color: primeColor,
              ),),
            ),
+            SwitchListTile(
+                title: Text("Permitir alertas de ofertas",
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  color: primeColor
+                ),),
+                activeColor: primeColor,
+                secondary: Icon(Icons.add_alert,
+                color: primeColor,),
+                value: _choiseKey,
+                onChanged: (bool? vlr1){
+                  setState(() {
+                    _choiseKey = vlr1!;
+                  });
+                },
+            ),
+            SwitchListTile(
+                title: Text("Sincronizar com sua conta Google",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: primeColor
+                ),),
+                activeColor: primeColor,
+                secondary: Icon(Icons.account_circle,
+                color: primeColor,),
+                value: _choiseKey2,
+                onChanged: (bool? vlr2){
+                  setState(() {
+                    _choiseKey2 = vlr2!;
+                  });
+                },
+            ),
           ],
         ),
       ),

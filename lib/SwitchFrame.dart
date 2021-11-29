53d3621224259f16738f421c3aa00678938e5d5c
GuiQRSM <endbeob15@gmail.com>
2021-11-29 17:53:31 -0300

    Implements in Scaffold that the stful structure returns the bottomNavigationBar attribute with the BottonAppBar() class embedded, and puts the entire Column inside a SingleChildScrollView() class on the Container declared in the body attribute.

--- lib/SwitchFrame.dart
+++ lib/SwitchFrame.dart
@@ -27,112 +27,157 @@ class _SwitchFrameState extends State<SwitchFrame> {
       body: Container(
         padding: EdgeInsets.all(10),
         width: double.infinity,
-        child: Column(
-          children: <Widget>[
-           Padding(
-               padding: EdgeInsets.only(bottom: 14),
-           child:  Text("Configurações gerais",
-             style: TextStyle(
-               fontSize: 22,
-               fontWeight: FontWeight.w500,
-               color: primeColor,
-             ),),
-           ),
-            SwitchListTile(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(bottom: 14),
+                child:  Text("Configurações gerais",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),),
+              ),
+              SwitchListTile(
                 title: Text("Permitir alertas de ofertas.",
-                style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  color: primeColor
-                ),),
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: primeColor
+                  ),),
                 activeColor: primeColor,
                 secondary: Icon(Icons.add_alert,
-                color: primeColor,),
+                  color: primeColor,),
                 value: _choiseKey,
                 onChanged: (bool? vlr1){
                   setState(() {
                     _choiseKey = vlr1!;
                   });
                 },
-            ),
-            SwitchListTile(
+              ),
+              SwitchListTile(
                 title: Text("Sincronizar com sua conta Google.",
-                style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    color: primeColor
-                ),),
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: primeColor
+                  ),),
                 activeColor: primeColor,
                 secondary: Icon(Icons.account_circle,
-                color: primeColor,),
+                  color: primeColor,),
                 value: _choiseKey2,
                 onChanged: (bool? vlr2){
                   setState(() {
                     _choiseKey2 = vlr2!;
                   });
                 },
-            ),
-            SwitchListTile(
-                 title: Text("Permitir acesso a aplicativos de corrida.",
-                 style: TextStyle(
-                     fontWeight: FontWeight.w600,
-                     color: primeColor
-                 ),),
+              ),
+              SwitchListTile(
+                title: Text("Permitir acesso a aplicativos de corrida.",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      color: primeColor
+                  ),),
                 activeColor: primeColor,
                 secondary: Icon(Icons.local_taxi,
-                color: primeColor,),
+                  color: primeColor,),
                 value: _choiseKey3,
                 onChanged: (bool? vlr3){
-                   setState(() {
-                     _choiseKey3 = vlr3!;
-                   });
+                  setState(() {
+                    _choiseKey3 = vlr3!;
+                  });
                 },
-            ),
-            SwitchListTile(
+              ),
+              SwitchListTile(
                 title: Text("Ativar modo Dark.",
-                style: TextStyle(
+                  style: TextStyle(
                     fontWeight: FontWeight.w600,
                     color: primeColor,
-                ),),
+                  ),),
                 activeColor: primeColor,
                 secondary: Icon(Icons.dark_mode,
-                color: primeColor,),
+                  color: primeColor,),
                 value: _choiseKey4,
                 onChanged: (bool? vlr4){
                   setState(() {
                     _choiseKey4 = vlr4!;
                   });
                 },
-            ),
-            Padding(
-              padding: EdgeInsets.only(top:20),
-            child:  RaisedButton(
-              padding: EdgeInsets.all(14),
-              onPressed: (){
-                setState(() {
-                  _textDinamic = "Ativação do Switch 1: $_choiseKey\n"
-                                 "Ativação do Switch 2: $_choiseKey2\n"
-                                 "Ativação do Switch 2: $_choiseKey3\n"
-                                 "Ativação do Switch 2: $_choiseKey4\n";
-                });
-              },
-              child: Text("Salvar",
-                style: TextStyle(
-                  fontSize: 14,
-                  fontWeight: FontWeight.w400,
-                  color: Colors.white,
-                ),),
-              color: primeColor,
-            ),),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-            child:  Text(
-                "$_textDinamic",
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top:20),
+                child:  RaisedButton(
+                  padding: EdgeInsets.all(14),
+                  onPressed: (){
+                    setState(() {
+                      _textDinamic = "Ativação do Switch 1: $_choiseKey\n"
+                          "Ativação do Switch 2: $_choiseKey2\n"
+                          "Ativação do Switch 2: $_choiseKey3\n"
+                          "Ativação do Switch 2: $_choiseKey4\n";
+                    });
+                  },
+                  child: Text("Salvar",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white,
+                    ),),
                   color: primeColor,
                 ),),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child:  Text(
+                  "$_textDinamic",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    color: primeColor,
+                  ),),
+              ),
+            ],
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: primeColor,
+        child: Padding(
+          padding: EdgeInsets.all(14),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                "b1",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "b2",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "b3",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                "b4",
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );

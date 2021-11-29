21f8de45e19c4829eafefd109d06cc8e1de1844d
GuiQRSM <endbeob15@gmail.com>
2021-11-29 16:20:39 -0300

    Implement a new Dart file named SwitchFrame in the lib folder, with stful already defined in it.

---
+++ lib/SwitchFrame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SwitchFrame extends StatefulWidget {
+  const SwitchFrame({Key? key}) : super(key: key);
+
+  @override
+  _SwitchFrameState createState() => _SwitchFrameState();
+}
+
+class _SwitchFrameState extends State<SwitchFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

05ff44b571053728a25ad0a94b53d851b9ba5551
GuiQRSM <endbeob15@gmail.com>
2021-12-13 11:33:34 -0300

    Implement in the 'lib' folder the 'GetApiFrame' dart file already configured with the 'import 'package:flutter/material.dart';' and with the 'stful' structure returning a 'Scaffold()' class.

---
+++ lib/GetApiFrame.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class GetApiFrame extends StatefulWidget {
+  const GetApiFrame({Key? key}) : super(key: key);
+
+  @override
+  _GetApiFrameState createState() => _GetApiFrameState();
+}
+
+class _GetApiFrameState extends State<GetApiFrame> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

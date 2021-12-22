1826ef5bc4b9cdb2a8b21b4f97f227d2f516edd3
GuiQRSM <endbeob15@gmail.com>
2021-12-22 12:50:01 -0300

    Insert in the 'lib' folder a dart file called 'ReceiverList_Frame' with the 'import' material.dart, http.dart, dart:convert called and the 'stful' structure returned to the 'Scaffold()' class implemented

---
+++ lib/ReceiverList_Frame.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class ReceiverList extends StatefulWidget {
+  const ReceiverList({Key? key}) : super(key: key);
+
+  @override
+  _ReceiverListState createState() => _ReceiverListState();
+}
+
+class _ReceiverListState extends State<ReceiverList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

f2a55fd85820978e101820e0b1c85a5f4980d014
GuiQRSM <endbeob15@gmail.com>
2021-12-29 15:20:11 -0300

    Implements the 'appBar:,' attribute in the 'Scaffold()' class, receiving the 'AppBar()' class with the 'title:,' 'backgroundColor:,' and 'actions: <> [ ]' attributes, taking the 'IconButton' classes ()'

--- lib/MainNavigation_Frame.dart
+++ lib/MainNavigation_Frame.dart
@@ -7,9 +7,42 @@ class MainNavigation extends StatefulWidget {
   _MainNavigationState createState() => _MainNavigationState();
 }
 
+const primeColor = Color.fromARGB(255, 98, 0, 238);
+const supportingColor = Color.fromRGBO(246, 203, 237, 1);
+
 class _MainNavigationState extends State<MainNavigation> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primeColor,
+        title: Text(
+            "E-conomiz",
+          style: TextStyle(
+            color: supportingColor,
+          ),
+        ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(
+              Icons.notifications,
+              color: supportingColor,
+            ),
+            onPressed: (){
+              print("ícone de notificações ativada!");
+            },
+          ),
+          IconButton(
+            icon: Icon(
+              Icons.alternate_email_rounded,
+              color: supportingColor,
+            ),
+            onPressed: (){
+              print("ícone de e-mail ativado!");
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
